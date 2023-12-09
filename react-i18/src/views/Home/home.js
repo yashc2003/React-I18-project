8c04745048f66d8eaa4dc131f9b92dcf46990141
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-12-09 09:34:38 +0000

    update file

--- react-i18/src/views/Home/home.js
+++ react-i18/src/views/Home/home.js
@@ -3,34 +3,16 @@ import './home.css';
 
 
 function Home() {
-    const users = 15;
-
     return (
+        <div>
+            <h1>Welcome !</h1>
+            <p>Wehope you enjoy learning React.</p>
+            <h1>
+                Have a great day!
+            </h1>
+        </div>
     
-             <div className="language-con">  
-            <h1 className="main-heading">{I18n("welcomemessage")}</h1>
-            <p className="normal-text">{I18n("normalmessage")}</p>
-            <h3 className="sub-heading">{I18n("greetingmessage")}</h3>
-
-
-            <div className="language-container">
-           
-        <select
-          className="language-select"
-          defaultValue={localStorage.getItem("lang")}
-          onChange={(e) => {
-            localStorage.setItem("lang", e.target.value);
-            window.location.reload();
-          }}
-        >
-          
-          <option value="mr" className="marathi-option">Marathi</option>
-          <option value="hi" className="hindi-option">Hindi</option>
-          <option value="en" className="english-option">English</option>
-          
-        </select>
-      </div>
-      </div>
+        
     );
 }
 
