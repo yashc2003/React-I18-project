a6ab6abc302e90457da72e3facd158a34cb86347
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-12-09 10:01:09 +0000

    update file

--- react-i18/src/views/Home/home.js
+++ react-i18/src/views/Home/home.js
@@ -3,13 +3,18 @@ import './home.css';
 
 
 function Home() {
+    const usersCount =15;
     return (
         <div>
             <h1>Welcome !</h1>
-            <p>Wehope you enjoy learning React.</p>
-            <h1>
-                Have a great day!
-            </h1>
+
+            <p>We hope you enjoy learning React.</p>
+
+            <h3>Have a great day!</h3>
+
+            <p>
+                {usersCount} users are learning in this session.
+            </p>
         </div>
     
         
